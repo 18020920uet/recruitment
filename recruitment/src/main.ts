6f8d04045f49e1ebb000032f690dd6df597423d4
inFngNam <inf.ng.nam@gmail.com>
2021-10-06 16:28:43 +0700

    Add HttpExceptionFilter

--- recruitment/src/main.ts
+++ recruitment/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { AppModule } from './app.module';
+
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT);
 }
 bootstrap();
