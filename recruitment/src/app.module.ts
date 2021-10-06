84d5583f6628a219fb43dcd58bb335cdd7a0d920
inFngNam <inf.ng.nam@gmail.com>
2021-10-06 10:11:17 +0700

    Init TypeOrm

--- recruitment/src/app.module.ts
+++ recruitment/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,6 +12,7 @@ import configuration from './config/configuration';
     ConfigModule.forRoot({
       load: [configuration]
     }),
+    TypeOrmModule.forRoot(),
     AuthModule
   ],
   controllers: [AppController],
--- recruitment/src/config/configuration.ts
+++ recruitment/src/config/configuration.ts
@@ -1,7 +1,3 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
-  // database: {
-  //   host: process.env.DATABASE_HOST,
-  //   port: parseInt(process.env.DATABASE_PORT, 10) || 5432
-  // }
 });
